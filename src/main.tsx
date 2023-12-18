c0eff3251fac270f5246fa4bd6a82fae2627dcee
Igor Komendantov <komendantov2580@gmail.com>
2023-12-18 18:55:06 +0200

    apply Chakra provider to app

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from 'app'
 import 'app/providers/localization'
+import { ChakraProvider } from 'app/providers/chakra'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
