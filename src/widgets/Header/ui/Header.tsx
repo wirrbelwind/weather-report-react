d2e7790be7a3b3f0f7a5b60f6db30214623132b7
Igor Komendantov <komendantov2580@gmail.com>
2023-12-18 22:35:05 +0200

    add widget/Header

---
+++ src/widgets/Header/ui/Header.tsx
@@ -0,0 +1,17 @@
+import { Box, Container, HStack } from "@chakra-ui/react"
+import { LangSwitcher } from "features/language-switching"
+import { ThemeSwitcher } from "features/theme-switching"
+
+export const Header = () => {
+
+	return (
+		<Box borderBottom="1px solid black">
+			<Container size="md">
+				<HStack justifyContent="flex-end" gap="1rem">
+					<ThemeSwitcher />
+					<LangSwitcher />
+				</HStack>
+			</Container>
+		</Box>
+	)
+}
