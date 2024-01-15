1355e910377003d92b25ddf2b434649d94a79c5c
Igor Komendantov <komendantov2580@gmail.com>
2024-01-15 23:22:41 +0200

    fix: fix i18n

--- public/locales/en/index.ts
+++ public/locales/en/index.ts
@@ -2,10 +2,12 @@ import globalTranslation from './translation.json'
 import header from './header.json'
 import weatherParams from './weatherParams.json'
 import weatherTypes from './weatherTypes.json'
+import langs from './langs.json'
 
 export const en = {
 	translation: globalTranslation,
 	header: header,
 	weatherParams,
-	weatherTypes
+	weatherTypes,
+	langs
 } as const
--- public/locales/ua/index.ts
+++ public/locales/ua/index.ts
@@ -2,10 +2,12 @@ import globalTranslation from './translation.json'
 import header from './header.json'
 import weatherParams from './weatherParams.json'
 import weatherTypes from './weatherTypes.json'
+import langs from './langs.json'
 
 export const ua = {
 	translation: globalTranslation,
 	header,
 	weatherParams,
-	weatherTypes
+	weatherTypes,
+	langs
 } as const
