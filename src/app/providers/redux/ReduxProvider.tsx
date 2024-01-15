09f5a8572703d593214a0107dfbea1031abbf78e
Igor Komendantov <komendantov2580@gmail.com>
2024-01-15 16:09:12 +0200

    fix: fix naming in redux provider

--- src/app/providers/redux/ReduxProvider.tsx
+++ src/app/providers/redux/ReduxProvider.tsx
@@ -1,5 +1,5 @@
 import { Provider } from "react-redux"
-import { globalStore} from "./globalStore"
+import { globalStore } from "./globalStore"
 import { ProviderProps } from "../ProviderProps"
 
 export const ReduxProvider = (props: ProviderProps) => {
