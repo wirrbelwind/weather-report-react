25d622f9dea0df6ce6a0e48888d046fea100fa9e
Igor Komendantov <komendantov2580@gmail.com>
2024-01-15 15:15:10 +0200

    feat: expand api type of weather response

--- src/entities/weather/types/WeatherForecastResponse.ts
+++ src/entities/weather/types/WeatherForecastResponse.ts
@@ -7,29 +7,62 @@ export interface WeatherForecastResponse {
 	timezone: string
 	timezone_abbreviation: string
 	elevation: number
-
+	// current
 	current_units: {
 		time: string
 		interval: string
 		temperature_2m: string
 		relative_humidity_2m: string
+		apparent_temperature: string
+		rain: string
+		showers: string
+		snowfall: string
+		wind_speed_10m: string
 	}
 	current: {
-		time: string
+		time: number
 		interval: number
 		temperature_2m: number
 		relative_humidity_2m: number
+		rain: number
+		showers: number
+		snowfall: number
+		wind_speed_10m: number
 	}
-
+	// daily
 	daily_units: {
 		time: string
 		temperature_2m_max: string
 		temperature_2m_min: string
+		sunrise: string
+		sunset: string
+		daylight_duration: string
+		sunshine_duration: string
+		uv_index_max: string
+		wind_speed_10m_max: string
+		wind_guests_10m_max: string
+
 	}
 	daily: {
-		time: string[],
+		time: number[],
 		temperature_2m_min: number[],
 		temperature_2m_max: number[]
+		sunrise: number[],
+		sunset: number[]
+		daylight_duration: number[]
+		sunshine_duration: number[]
+		uv_index_max: number[]
+		wind_speed_10m_max: number[]
+		wind_guests_10m_max: number[]
+	}
+	// hour
+	hourly_units: {
+		time: string
+		temperature_2m: string
+	}
+	hourly: {
+		time: number[]
+		temperature_2m: number[],
 	}
 }
 
