f9a55dcc48f480ceb68ff414793008b637f59aea
Igor Komendantov <komendantov2580@gmail.com>
2024-01-15 14:04:52 +0200

    feat: wrap repux provider around app

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { App } from 'app'
 import 'app/providers/localization'
 import { ChakraProvider } from 'app/providers/chakra'
+import { ReduxProvider } from 'app/providers/redux'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <ReduxProvider>
+        <App />
+      </ReduxProvider>
     </ChakraProvider>
   </React.StrictMode>,
 )
