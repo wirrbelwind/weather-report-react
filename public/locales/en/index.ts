2de029cd90d623e615c8dd946e1857234a08794a
Igor Komendantov <komendantov2580@gmail.com>
2024-01-15 22:59:19 +0200

    fix: add ns to i18n

--- public/locales/en/index.ts
+++ public/locales/en/index.ts
@@ -1,6 +1,11 @@
 import globalTranslation from './translation.json'
 import header from './header.json'
+import weatherParams from './weatherParams.json'
+import weatherTypes from './weatherTypes.json'
+
 export const en = {
 	translation: globalTranslation,
-	header: header
+	header: header,
+	weatherParams,
+	weatherTypes
 } as const
--- public/locales/ua/index.ts
+++ public/locales/ua/index.ts
@@ -1,6 +1,11 @@
 import globalTranslation from './translation.json'
 import header from './header.json'
+import weatherParams from './weatherParams.json'
+import weatherTypes from './weatherTypes.json'
+
 export const ua = {
 	translation: globalTranslation,
-	header: header
+	header,
+	weatherParams,
+	weatherTypes
 } as const
